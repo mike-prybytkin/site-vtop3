9670fd5aff428678f5264a2a33a9325d099d0a90
@Mike-Prybytkin <prybytkin@bk.ru>
2023-07-21 20:52:29 +0300

    feat: add hover-shadow to button and refactor promoSwiper

--- src/script/promo-swipew/promo-swiper.js
+++ src/script/promo-swipew/promo-swiper.js
@@ -1,13 +1,11 @@
 import Swiper from 'swiper';
 import { Autoplay } from 'swiper/modules';
 
-const promoSwiper = () => {
-  const swiper = new Swiper('.promo-swiper', {
+const promoSwiper = () =>
+  new Swiper('.promo-swiper', {
     modules: [Autoplay],
     loop: true,
     autoplay: { delay: 5000 },
   });
-  swiper.start();
-};
 
 export default promoSwiper;
