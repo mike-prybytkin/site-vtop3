ed22d203610d88c5bdf3576218d8aabe9e039cfb
@Mike-Prybytkin <prybytkin@bk.ru>
2023-08-07 22:39:42 +0300

    refactor: add fade-effect to promo-swiper

--- src/script/promo-swipew/promo-swiper.js
+++ src/script/promo-swipew/promo-swiper.js
@@ -1,11 +1,13 @@
 import Swiper from 'swiper';
-import { Autoplay } from 'swiper/modules';
+import { Autoplay, EffectFade } from 'swiper/modules';
 
 const promoSwiper = () =>
   new Swiper('.promo-swiper', {
-    modules: [Autoplay],
+    modules: [Autoplay, EffectFade],
     loop: true,
-    // autoplay: { delay: 5000 },
+    autoplay: { delay: 5000 },
+    effect: 'fade',
+    fadeEffect: { crossFade: true },
   });
 
 export default promoSwiper;
